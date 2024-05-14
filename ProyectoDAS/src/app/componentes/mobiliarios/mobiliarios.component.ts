7a4492c2bd17000fc170e7a6406c851b2b1437cf
AngelCaiza <acaiza0272@uta.edu.ec>
2024-05-13 21:33:26 -0500

    Se realizo el metodo de buscar por nombre un bien mobiliario

--- ProyectoDAS/src/app/componentes/mobiliarios/mobiliarios.component.ts
+++ ProyectoDAS/src/app/componentes/mobiliarios/mobiliarios.component.ts
@@ -13,6 +13,7 @@ export class MobiliariosComponent {
   selectedCity: any;
   tooltipVisible: boolean = false;
   visible: boolean = false;
+  nombreBuscado:string='';
         constructor(private mobiliariosService:MobiliariosService) { 
           this.listarMobiliario()
         }
@@ -34,6 +35,24 @@ export class MobiliariosComponent {
         console.error('Error al obtener mobiliarios:', error);
       }
     )
+  }
+
+  cargarMobiliario(nombre: string): void {
+    this.mobiliariosService.obtenerUsuarioPorNombre(nombre).subscribe(
+      (response: any) => {
+        if (response && response.mobiliarios) {
+          this.mobiliarios = response.mobiliarios;
+        } else {
+          this.mobiliarios = [];
+        }
+      },
+      (error) => {
+        console.error('Usuario no encontrado:', error);
+      }
+    );
+  }
+  buscarMobiliario() {
+    this.cargarMobiliario(this.nombreBuscado);
   }
 
   showTooltip() {
--- ProyectoDAS/src/app/services/mobiliarios.service.ts
+++ ProyectoDAS/src/app/services/mobiliarios.service.ts
@@ -1,13 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
 @Injectable({
   providedIn: 'root'
 })
 export class MobiliariosService {
   API = "http://localhost:4000/api/mobiliarios"
-  
+
   constructor(private http: HttpClient) {}
   obtenerMobiliarios() {
     return this.http.get(this.API);
+  }
+
+  obtenerUsuarioPorNombre(nombre: string): Observable<any[]>{
+    return this.http.get<any[]>(`${this.API}/nombre/${nombre}`);
   }
 }
