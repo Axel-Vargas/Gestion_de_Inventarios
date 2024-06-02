12ed06a927dc8d075d89ba7cde7013f9ce83f791
Axel Vargas <axelvargasvilla1709@outlook.com>
2024-06-02 16:54:17 -0500

    Se realizó el módulo de gestión de Proveedores

---
+++ ProyectoDAS/src/app/services/proveedores.service.ts
@@ -0,0 +1,28 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProveedoresService {
+
+  API = "http://localhost:4000/api/proveedores"
+
+  constructor(private http: HttpClient) { }
+
+  obtenerProveedores() {
+    return this.http.get(this.API);
+  }
+
+  insertarProveedor(nombre: string, direccion: string, telefono: string ) {
+    return this.http.post(this.API, { nombre, direccion, telefono })
+  }
+
+  actualizarProveedor(id: String, nombre: string, direccion: string, telefono: string ) {
+    return this.http.put(`${this.API}/${id}`, { nombre, direccion, telefono  })
+  }
+
+  eliminarProveedor(id: string) {
+    return this.http.delete(`${this.API}/${id}`);
+  }
+}
