7f24431c19dd80fa1c30ce71bb744cd56353e01c
David Flores <eflores9091@uta.edu.ec>
2024-06-05 02:50:26 -0500

    Se modificaron las rutas de backend para las consultas del dashboard

--- appServidor/src/routes/inicioRoutes.js
+++ appServidor/src/routes/inicioRoutes.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 const { getTotalBienes, getTotalAreas, getTotalProveedores, getTotalUsuarios} = require('../controllers/inicioController.js');
 
-router.get('/', getTotalBienes);
-router.get('/', getTotalAreas);
-router.get('/', getTotalProveedores);
-router.get('/', getTotalUsuarios);
+router.get('/total-bienes', getTotalBienes);
+router.get('/total-areas', getTotalAreas);
+router.get('/total-proveedores', getTotalProveedores);
+router.get('/total-usuarios', getTotalUsuarios);
 
 module.exports = router;
