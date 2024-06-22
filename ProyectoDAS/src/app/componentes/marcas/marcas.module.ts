aa5dccbed0d4157b2d9a0f68ae14968ee654eecc
David Flores <eflores9091@uta.edu.ec>
2024-06-22 00:32:03 -0500

    importacion de modulos de primeng

--- ProyectoDAS/src/app/componentes/marcas/marcas.module.ts
+++ ProyectoDAS/src/app/componentes/marcas/marcas.module.ts
@@ -3,7 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { MarcasRoutingModule } from './marcas-routing.module';
 import { MarcasComponent } from './marcas.component';
-
+import { TableModule } from 'primeng/table';
+import { DialogModule } from 'primeng/dialog';
+import { ButtonModule } from 'primeng/button';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { ToastModule } from 'primeng/toast';
+import { DropdownModule } from 'primeng/dropdown';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import {MessagesModule} from 'primeng/messages';
+import { DialogService } from 'primeng/dynamicdialog';
+import { ConfirmationService } from 'primeng/api';
 
 @NgModule({
   declarations: [
@@ -11,10 +21,24 @@ import { MarcasComponent } from './marcas.component';
   ],
   imports: [
     CommonModule,
-    MarcasRoutingModule
+    MarcasRoutingModule,
+    TableModule,
+    DialogModule,
+    ButtonModule,
+    FormsModule,
+    HttpClientModule,
+    ToastModule,
+    DropdownModule,
+    MessagesModule,
+    ConfirmDialogModule
   ],
   exports: [
     MarcasComponent
+  ],
+  providers: [
+    DialogService,
+    ConfirmationService,
+    MessagesModule
   ]
 })
 export class MarcasModule { }
