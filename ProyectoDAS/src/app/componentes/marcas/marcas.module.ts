31a37579170459ba8232b330b3a2b08043682445
David Flores <eflores9091@uta.edu.ec>
2024-06-22 00:22:40 -0500

    Se genero el componente de marcas

---
+++ ProyectoDAS/src/app/componentes/marcas/marcas.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MarcasRoutingModule } from './marcas-routing.module';
+import { MarcasComponent } from './marcas.component';
+
+
+@NgModule({
+  declarations: [
+    MarcasComponent
+  ],
+  imports: [
+    CommonModule,
+    MarcasRoutingModule
+  ],
+  exports: [
+    MarcasComponent
+  ]
+})
+export class MarcasModule { }
