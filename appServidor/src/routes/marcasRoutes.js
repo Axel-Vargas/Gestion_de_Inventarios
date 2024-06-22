484d4e111a7c1f299cf16bb5de4803d567b4b36d
David Flores <eflores9091@uta.edu.ec>
2024-06-22 00:18:34 -0500

    se crearon los controller y rutas para marcas

--- appServidor/src/routes/marcasRoutes.js
+++ appServidor/src/routes/marcasRoutes.js
@@ -2,8 +2,11 @@ const express = require('express');
 const router = express.Router();
 const marcasController = require('../controllers/marcasController');
 
+// Rutas para las marcas
 router.get('/', marcasController.getAllMarcas);
 router.post('/', marcasController.createMarcas);
+router.get('/tecnologicos', marcasController.geMarcasTecnologicos); 
+router.get('/mobiliarios', marcasController.geMarcasMobiliarios); 
 router.get('/:id', marcasController.getMarcasById);
 router.put('/:id', marcasController.updateMarcas);
 router.delete('/:id', marcasController.deleteMarcas);
